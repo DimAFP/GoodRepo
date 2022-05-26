466e618903a1e454a3075785bbe5983bc21a798c
Apprenant 08 <apprenant08@macbook-pro-de-apprenant.home>
2022-05-26 10:22:07 +0200

    first

---
+++ GoodRepo/GoodRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodRepoApp.swift
+//  GoodRepo
+//
+//  Created by Apprenant 08 on 26/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct GoodRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
